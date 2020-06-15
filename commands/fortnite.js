f2b18e76534606a6f0badd074f7d99f2213005f7
Glitch (hello-express) <none>
2020-06-15 14:27:37 +0000

    😀🌲 Checkpoint ./commands/fortnite.js:17590094/317

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -1,18 +1,26 @@
 const Fortnite = require('fortnite');
 const stats = new Fortnite()
+const Discord = require('Discord.js');
 
 exports.run = (client, message, args, tools) => {
   
   let platform;
   let username;
  
-  if (!['pc', 'xbl', 'psn'].includes(args)) message.channel.send('**Please include the plataform: `!fortnite <plataform> <username>`**');
-  if (!args[1]) message.channel.send('**Please include the username: `!fortnite <platform> <username>`**');
+  if (!['pc', 'xbl', 'psn'].includes(args[0])) message.channel.send('**Please include the plataform: `!fortnite [pc | xbl | psn ] <username>`**');
+  if (!args[1]) message.channel.send('**Please include the username: `!fortnite [pc | xbl | psn] <username>`**');
   platform = args.shift();
   username = args.join(' ');
   
   stats.getInfo(username, platform).then( data => {
     
+    const embed = new Discord.MessageEmbed()
+      .setColor(0xffffff)
+    
+  })
+  .catch(error => {
+    
+    message.channel.send('Username not found!');
   })
   
   
