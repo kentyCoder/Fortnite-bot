938e13ae6bc96fe98423c9aa06054678396fe93b
Glitch (hello-express) <none>
2020-06-15 15:36:56 +0000

    🎀🌽 Checkpoint ./commands/fortnite.js:17590094/243

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -17,7 +17,9 @@ exports.run = (client, message, args, tools) => {
     const embed = new Discord.MessageEmbed()
       .setColor(0xffffff)
       .setTitle(`Stats for ${data.username}`)
-      .setDescription(`**Top Placement, `**Top 3:** *${data.lifetimeStats[0].value}*\n**Top 5:** *${data.lifetimeStats[1].value}*\n**Top 6:** *${data.lifetimeStats[3].value}*\n**Top 12:** *${data.lifetimeStats[4].value}*\n**Top 25:** *${data.lifetimeStats[5].value}*`, true)
+      .setDescription(`**Top Placement**\n\n**Top 3:** *${data.lifetimeStats[0].value}*\n**Top 5:** *${data.lifetimeStats[1].value}*\n**Top 6:** *${data.lifetimeStats[3].value}*\n**Top 12:** *${data.lifetimeStats[4].value}*\n**Top 25:** *${data.lifetimeStats[5].value}*`, true)
+    
+    .setThumbnail('https://www.google.com/search?q=fortnite+logo&rlz=1CALFJO_enES867&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjGqvmSkYTqAhW5DWMBHb88AY4Q_AUoAXoECA4QAw&biw=1300&bih=629&safe=active&ssui=on#imgrc=3qudz9De99BlaM')
     .addField('Total Score', data.lifetimeStats[6].value)
     .addField('Matches Played', data.lifetimeStats[7].value, true)
     .addfield('Wins', data.lifetimeStats[8].value, true)
