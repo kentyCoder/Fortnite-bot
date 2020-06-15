9bbe282e87250ae067a002552b1487ce10e7c0fb
Glitch (hello-express) <none>
2020-06-15 15:25:26 +0000

    ✌️🍣 Checkpoint ./commands/fortnite.js:17590094/681

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -17,9 +17,18 @@ exports.run = (client, message, args, tools) => {
     const embed = new Discord.MessageEmbed()
       .setColor(0xffffff)
       .setTitle(`Stats for ${data.username}`)
-      .addField('Top Placement', `**Top 3:** *${data.lifetimeStats[0].value}*\n**Top 5:** *${data.lifetimeStats[1].value}*\n**Top 6:** *${data.lifetimeStats[3].value}*\n**Top 12:** *${data.lifetimeStats[4].value}*\n**Top 25:** *${data.lifetimeStats[5].value}*`)
-    .addField('Total score', data.lifetimeStats[6].value)
-    .addField('Matches Played', data.lifetimeStats)
+      .setDescription(`**Top Placement, `**Top 3:** *${data.lifetimeStats[0].value}*\n**Top 5:** *${data.lifetimeStats[1].value}*\n**Top 6:** *${data.lifetimeStats[3].value}*\n**Top 12:** *${data.lifetimeStats[4].value}*\n**Top 25:** *${data.lifetimeStats[5].value}*`, true)
+    .addField('Total Score', data.lifetimeStats[6].value)
+    .addField('Matches Played', data.lifetimeStats[7].value, true)
+    .addfield('Wins', data.lifetimeStats[8].value, true)
+    .addfield('Win Percentage', data.lifetimeStats[9].value, true)
+    .addfield('Kills', data.lifetimeStats[10].value, true)
+    .addfield('K/D Ratio', data.lifetimeStats[11].value, true)
+    .addfield('Kills Per Minute', data.lifetimeStats[12].value, true)
+    .addfield('Time Played', data.lifetimeStats[13].value, true)
+    .addfield('Average Survival Time', data.lifetimeStats[14].value, true)
+    
+    message.channel.send(embed)
     
   })
   .catch(error => {
