8358ff5d111ec31306a238e4ffd58126535533e3
Glitch (hello-express) <none>
2020-06-15 14:16:12 +0000

    🚒🍠 Checkpoint ./commands/fortnite.js:17590094/585

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -3,6 +3,17 @@ const stats = new Fortnite()
 
 exports.run = (client, message, args, tools) => {
   
-  let 
+  let platform;
+  let username;
+ 
+  if (!['pc', 'xbl', 'psn'].includes(args)) message.channel.send('**Please include the plataform: `!fortnite <plataform> <username>`**');
+  if (!args[1]) message.channel.send('**Please include the username: `!fortnite <platform> <username>`**');
+  platform = args.shift();
+  username = args.join(' ');
+  
+  stats.getInfo(username, platform).then( data => {
+    
+  })
+  
   
 }
